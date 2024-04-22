b3a802605be80b3dbb3ba533a66250a583b9ca8c
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-22 08:43:42 +0000

    link added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 
 const HomePage = () => {
+  const router = useRouter();
+
   return (
     <main className="dark:bg-[#0D171E] h-full w-full flex flex-col gap-y-5 items-center justify-center">
       <h1 className="text-4xl">Hello, World!</h1>
 
-      <Button variant="default" size="lg">
-        Click me
+      <Button
+        onClick={() => router.push("/dashboard")}
+        variant="default"
+        size="lg"
+      >
+        Dashboard
       </Button>
 
       <ModeToggle />
