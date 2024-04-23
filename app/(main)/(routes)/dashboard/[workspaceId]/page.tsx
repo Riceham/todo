45fd9adf8ce591f6fa0e3c7c393350bad7e74689
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-23 10:48:33 +0000

    dashboard page design implemented

--- app/(main)/(routes)/dashboard/[workspaceId]/page.tsx
+++ app/(main)/(routes)/dashboard/[workspaceId]/page.tsx
@@ -1,5 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+
 const WorkspaceIdPage = () => {
-  return <div>WorkspaceIdPage</div>;
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        alt="Empty"
+        height={400}
+        width={400}
+        className="dark:hidden select-none pointer-events-none"
+        draggable={false}
+      />
+      <Image
+        src="/empty-dark.png"
+        alt="Empty"
+        height={400}
+        width={400}
+        className="hidden dark:block select-none pointer-events-none"
+        draggable={false}
+      />
+
+      <h2 className="text-2xl font-medium">
+        Create your first{" "}
+        <span className="text-primary font-bold">Todo list</span> to get
+        started.
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create Todo
+      </Button>
+    </div>
+  );
 };
 
 export default WorkspaceIdPage;
--- app/(main)/(routes)/dashboard/page.tsx
+++ app/(main)/(routes)/dashboard/page.tsx
@@ -1,5 +1,38 @@
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+
 const DashboardPage = () => {
-  return <div className="">Dashboard Page</div>;
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        alt="Empty"
+        height={400}
+        width={400}
+        className="dark:hidden select-none pointer-events-none"
+        draggable={false}
+      />
+      <Image
+        src="/empty-dark.png"
+        alt="Empty"
+        height={400}
+        width={400}
+        className="hidden dark:block select-none pointer-events-none"
+        draggable={false}
+      />
+
+      <h2 className="text-2xl font-medium">
+        Welcome to <span className="text-primary font-bold">CountWave</span>.
+      </h2>
+
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Add Workspace
+      </Button>
+    </div>
+  );
 };
 
 export default DashboardPage;
