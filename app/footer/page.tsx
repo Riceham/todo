18d6f385512fc6267695d080bac25a84f53e7c79
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-27 07:18:41 +0000

    removed unwanted assets

--- app/footer/page.tsx
+++ app/footer/page.tsx
@@ -1,11 +1,9 @@
 const Footer = () => {
   return (
-    <footer
-      className=" border-t-[1px] hidden md:block"
-    >
+    <footer className="border-t-[1px] text-white hidden md:block">
       <div className="flex justify-evenly gap-4 p-4">
         <div>
-          <div className="flex flex-col p-4 cursor-pointer text-xl">
+          <div className="flex flex-col p-4 text-xl">
             All rights reserved. @2023 by Bird Inc.
           </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import Carousel from "@/components/carousel";
 import Firstsection from "./firstsection/page";
 import Navbar from "./navbar/page";
-import FifthSection from "./fifthsection/page";
-import Footer from "./footer/page";
-import FourthSection from "./fourthsection/page";
-import SixthSection from "./sixthsection/page";
-import ThirdSection from "./thirdsection/page";
-import SecondSection from "./secondsection/page";
 import Pricing from "./pricing/page";
+import SixthSection from "./sixthsection/page";
 
 const Home = () => {
   return (
