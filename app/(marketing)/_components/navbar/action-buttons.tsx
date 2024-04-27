a40f673aa216511b575cc07763279873b03e1e23
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-27 10:16:28 +0000

    navbar sign in state added

--- app/(marketing)/_components/navbar/action-buttons.tsx
+++ app/(marketing)/_components/navbar/action-buttons.tsx
@@ -1,20 +1,44 @@
+import { ArrowRight, CircleArrowUp, CircleGauge, LogIn } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
-import { ModeToggle } from "@/components/mode-toggle";
 
 export const ActionButtons = () => {
+  const isLoggedIn = false;
+
   return (
     <div className="flex items-center gap-x-2">
-      <Button>
-        <Link href="/dashboard">Dashboard</Link>
-      </Button>
-
-      <Button asChild>
-        <Link href="#pricing">Try CountWave</Link>
-      </Button>
-
-      <ModeToggle />
+      {isLoggedIn ? (
+        <>
+          <Button variant="outline" asChild>
+            <Link href="/dashboard">
+              <CircleGauge className="h-4 w-4 mr-1" />
+              Dashboard
+            </Link>
+          </Button>
+          <Button asChild>
+            <Link href="#pricing">
+              <CircleArrowUp className="h-4 w-4 mr-1" />
+              Upgrade
+            </Link>
+          </Button>
+        </>
+      ) : (
+        <>
+          <Button asChild>
+            <Link href="/sign-up">
+              Get started
+              <ArrowRight className="h-4 w-4 ml-1" />
+            </Link>
+          </Button>
+          <Button variant="outline" asChild>
+            <Link href="/sign-in">
+              <LogIn className="h-4 w-4 mr-1" />
+              Sign in
+            </Link>
+          </Button>
+        </>
+      )}
     </div>
   );
 };
--- app/(marketing)/_components/navbar/index.tsx
+++ app/(marketing)/_components/navbar/index.tsx
@@ -6,6 +6,7 @@ export const Navbar = () => {
   return (
     <header className="flex items-center justify-between px-10 border-b shadow-md fixed w-full py-1 bg-background">
       <Logo />
+
       <div className="flex items-center justify-between">
         <ActionButtons />
       </div>
