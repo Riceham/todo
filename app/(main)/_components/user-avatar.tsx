12f40610912f9860731016049c73ad0eefb2cb6a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-27 06:50:13 +0000

    overlay shadow added

--- app/(main)/_components/user-avatar.tsx
+++ app/(main)/_components/user-avatar.tsx
@@ -7,7 +7,7 @@ type UserAvatarProps = {
 
 export const UserAvatar = ({ src, alt }: UserAvatarProps) => {
   return (
-    <Avatar className="cursor-pointer border-[3px] border-primary/80">
+    <Avatar className="cursor-pointer border-[3px] border-primary/80 opacity-90 hover:opacity-100">
       <AvatarImage src={src} alt={alt} height={40} width={40} />
       <AvatarFallback className="bg-foreground/10">{alt[0]}</AvatarFallback>
     </Avatar>
