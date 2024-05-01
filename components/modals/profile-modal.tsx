cc74510cfebfb286cb97977e581ec4e7c458d744
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-01 07:47:56 +0000

    profile modal styles fixes

--- components/modals/profile-modal.tsx
+++ components/modals/profile-modal.tsx
@@ -9,17 +9,23 @@ export const ProfileModal = () => {
   const { theme } = useTheme();
   const clerkTheme = theme === "dark" ? dark : undefined;
   const textColor = theme === "light" ? "#000000" : "#FFFFFF";
+  const bgColor = theme === "dark" ? "#1f2937" : undefined;
 
   return (
-    <Dialog open={isOpen} onOpenChange={onClose} className="right-0">
-      <DialogContent className="bg-transparent p-0 flex items-center justify-center">
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent
+        className="bg-transparent p-0 flex items-center justify-center"
+        moveCloseButtonRight
+      >
         <div className={theme === "dark" ? "dark" : ""}>
           <UserProfile
             appearance={{
               baseTheme: clerkTheme,
               variables: {
-                colorText: textColor
-              }
+                colorText: textColor,
+                colorBackground: bgColor,
+                colorInputBackground: bgColor,
+              },
             }}
           />
         </div>
