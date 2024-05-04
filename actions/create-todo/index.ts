aa8edf6013e7dd3980c5348be0142847836ca7f4
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-04 14:28:38 +0000

    max free limit added

--- actions/create-todo/index.ts
+++ actions/create-todo/index.ts
@@ -3,8 +3,10 @@
 import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 
+import { MAX_FREE_TASKS } from "@/constants";
 import { createSafeAction } from "@/lib/create-safe-action";
 import { db } from "@/lib/db";
+import { checkSubscription } from "@/lib/subscription";
 import { CreateTodo } from "@/schema";
 
 import { InputType, ReturnType } from "./types";
@@ -40,6 +42,21 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     if (!workspace) {
       return {
         error: "Workspace not found.",
+      };
+    }
+
+    const isSubscribed = await checkSubscription();
+    const maxTodoCount = isSubscribed ? Infinity : MAX_FREE_TASKS;
+
+    const existingTodoCount = await db.todo.count({
+      where: {
+        workspaceId,
+      },
+    });
+
+    if (existingTodoCount + 1 > maxTodoCount) {
+      return {
+        error: "Upgrade to the pro plan to create more tasks.",
       };
     }
 
