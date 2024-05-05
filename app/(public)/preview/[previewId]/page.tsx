6bddf4c6bf762e196de3ed2b7f747812830077ca
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-05 08:05:35 +0000

    preview page completed

--- app/(public)/preview/[previewId]/page.tsx
+++ app/(public)/preview/[previewId]/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { notFound, redirect } from "next/navigation";
 
 import { TaskList } from "@/app/(main)/_components/task-list";
+import { ModeToggle } from "@/components/mode-toggle";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
@@ -41,19 +42,21 @@ const PreviewIdPage = async ({ params }: PreviewIdPageProps) => {
 
   return (
     <main className="mx-4">
-      <div className="flex items-center">
-        <div className="flex-[0.5]">
-          <Button asChild>
-            <Link href="/">
-              <Home className="h-4 w-4 mr-2" /> Home
-            </Link>
-          </Button>
-        </div>
-        <div className="flex items-center justify-center text-4xl py-4 gap-x-2">
-          <h1>{workspace.name}</h1>
+      <div className="flex items-center justify-between py-4">
+        <Button asChild>
+          <Link href="/">
+            <Home className="h-4 w-4 mr-2" /> Home
+          </Link>
+        </Button>
+
+        <div className="flex gap-x-2 items-center justify-center">
+          <h1 className="text-4xl">{workspace.name}</h1>
           <Badge variant="outline">Preview</Badge>
         </div>
+
+        <ModeToggle />
       </div>
+
       <TaskList workspaceId={workspace.id} todos={workspace.todos} isPreview />
     </main>
   );
